7723ea52eb5377ef7ebdf6aa0bd390df7930dd30
Giste <giste.trappiste@gmail.com>
2024-06-07 17:02:52 +0200

    Remove due to build errors.

--- kona.mk
+++ kona.mk
@@ -466,8 +466,8 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+#PRODUCT_PACKAGES += \
+#    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
